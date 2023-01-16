6d222d012020b7e322eff2040bfdc75984f895e4
Ozan Atici <ozanact@gmail.com>
2023-01-16 13:27:06 -0500

    new repo

---
+++ src/test/java/testng_knowledge/_03_Priority_and_Dependency.java
@@ -0,0 +1,30 @@
+package testng_knowledge;
+
+import org.testng.annotations.Test;
+
+public class _03_Priority_and_Dependency {
+  /*
+    @Test
+    public void login() {
+        System.out.println("This is login");
+    }
+
+    @Test
+
+    public void logout() {
+        System.out.println("This is logout");
+    }
+
+    @Test(dependsOnMethods = "logout")
+    public void forgotPassword() {
+        System.out.println("This is forgot password");
+    }
+
+    @Test(dependsOnMethods = {"login", "logOut", "forgotPassword"})
+    public void invalidLogin() {
+        System.out.println("This is invalid login");
+    }
+*/
+}
+
+
